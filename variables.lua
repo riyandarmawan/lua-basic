9420a354b96d8ba677beaab7066e779658c5762a
riyandarmawan <riyandarmawan@gmail.com>
2024-09-01 19:45:04 +0700

    variables

---
+++ variables.lua
@@ -0,0 +1,17 @@
+local x = 10 -- comment
+local name = "sid" -- string
+local isAlive = true -- boolean
+local a = nil -- no value or invalid value
+
+-- increment in numbers
+local n = 1
+n = n + 1
+print(n) -- 2
+
+-- strings 
+-- Concatenate strings
+local phrase = "I am"
+local name = "Sid"
+
+print(phrase .. " " .. name) -- I am Sid
+print("I am " .. "Sid")
